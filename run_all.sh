22ecb6c4923a12e6de8ffa072405234bc7d90626
Jonathan LEVY <jonathan.levy@ens-rennes.fr>
2018-09-04 12:54:50 +0200

    removed optirun. added correct run_all.sh. ignore .vscode folder.
    redirected output towards out.log.
    
    
    typo: ./configure => ./configure.sh
    
    
    run-all.sh cleaned up

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-./personal_configure.sh
-make clean
-make GPU_SM_ARCH=sm_20 MAX_SEQ_LEN=350 N_CODE=0x4E N_PENALTY=2
+./configure.sh /usr/local/cuda-9.2
+make GPU_SM_ARCH=sm_35 MAX_SEQ_LEN=300 N_CODE=0x4E N_PENALTY=-2
 cd test_prog
-make
 make $1 
-#cat out.log
 sha256sum -c crc_out.log.crc
+
 cd ..
