25df49897d988e647e25aed232cf1da5884267b8
Jonathan LEVY <jonathan.levy@ens-rennes.fr>
2018-09-23 20:37:44 +0200

    refactored code to iterate on query and target using pointers (no dupes)

--- src/gasal_kernels_inl.h
+++ src/gasal_kernels_inl.h
@@ -1,4 +1,8 @@
-#include <stdio.h>
+//#define DEBUG
+
+#ifdef DEBUG
+	#include <stdio.h>
+#endif
 
 #define A_PAK ('A'&0x0F)
 #define C_PAK ('C'&0x0F)
@@ -68,89 +72,130 @@ __global__ void	gasal_reversecomplement_kernel(uint32_t *packed_query_batch,uint
 		uint32_t target_batch_regs = (ref_len >> 3) + (ref_len&7 ? 1 : 0);//number of 32-bit words holding sequence of target_batch
 
 		uint32_t query_batch_regs_to_swap = (query_batch_regs >> 1) + (query_batch_regs & 1); // that's (query_batch_regs / 2) + 1 if it's odd, + 0 otherwise. Used for reverse (we start a both ends, and finish at the center of the sequence)
+		uint32_t target_batch_regs_to_swap = (target_batch_regs >> 1) + (target_batch_regs & 1); // that's (target_batch_regs / 2) + 1 if it's odd, + 0 otherwise. Used for reverse (we start a both ends, and finish at the center of the sequence)
 
 
-		if (query_op[tid] & 0x01) // reverse
+		// variables used dependent on target and query: 
+
+		uint8_t *op = NULL;
+		uint32_t *packed_batch = NULL;
+		uint32_t *batch_regs = NULL;
+		uint32_t *batch_regs_to_swap = NULL;
+		uint32_t *packed_batch_idx = NULL;
+
+		// avoid useless code duplicate thanks to pointers to route the data flow where it should be, twice.
+		#pragma unroll 2
+		for (int p = 0; p < 2; p++)
 		{
-			// deal with N's : read last word, find how many N's, store that number as offset, and pad with that many for the last 
-			uint8_t nbr_N = 0;
-			for (int j = 0; j < 32; j = j + 4)
+			switch(p)
 			{
-				nbr_N += (((packed_query_batch[packed_query_batch_idx + query_batch_regs-1] & (0x0F << j)) >> j) == N_PAK);
+				case QUERY:
+					op = query_op;
+					packed_batch = packed_query_batch;
+					batch_regs = &query_batch_regs;
+					batch_regs_to_swap = &query_batch_regs_to_swap;
+					packed_batch_idx = &packed_query_batch_idx;
+				break;
+				case TARGET:
+					op = target_op;
+					packed_batch = packed_target_batch;
+					batch_regs = &target_batch_regs;
+					batch_regs_to_swap = &target_batch_regs_to_swap;
+					packed_batch_idx = &packed_target_batch_idx;
+				break;
+				default:
+				break;
 			}
-			printf("KERNEL_DEBUG: nbr_N=%d\n", nbr_N);
-			nbr_N = nbr_N << 2; // we operate on nibbles so we will need to do our shifts 4 bits by 4 bits, so 4*nbr_N
 
-			for (uint32_t i = 0; i < (query_batch_regs_to_swap); i++) // reverse all words. There's a catch with the last word (in the middle of the sequence), see final if.
+			if (*(op + tid) & 0x01) // reverse
 			{
-				uint32_t rpac_1 = packed_query_batch[packed_query_batch_idx + i]; //load 8 packed bases from head
-				uint32_t rpac_2 = ((packed_query_batch[packed_query_batch_idx + query_batch_regs-2 - i]) << (32-nbr_N)) | ((packed_query_batch[packed_query_batch_idx + query_batch_regs-1 - i]) >> nbr_N);
-
-
-				uint32_t reverse_rpac_1 = 0;
-				uint32_t reverse_rpac_2 = 0;
-
-
-			#pragma unroll 8
-				for(int k = 28; k >= 0; k = k - 4)		// reverse 32-bits word... is pragma-unrolled. 
+				// deal with N's : read last word, find how many N's, store that number as offset, and pad with that many for the last 
+				uint8_t nbr_N = 0;
+				for (int j = 0; j < 32; j = j + 4)
 				{
-					reverse_rpac_1 |= ((rpac_1 & (0x0F << k)) >> (k)) << (28-k);
-					reverse_rpac_2 |= ((rpac_2 & (0x0F << k)) >> (k)) << (28-k);
+					nbr_N += (((*(packed_batch + *(packed_batch_idx) + *(batch_regs)-1) & (0x0F << j)) >> j) == N_PAK);
 				}
-				// last swap operated manually, because of its irregular size (32 - 4*nbr_N bits, hence 8 - nbr_N nibbles)
+				#ifdef DEBUG
+					printf("KERNEL_DEBUG: nbr_N=%d\n", nbr_N);
+				#endif
+				nbr_N = nbr_N << 2; // we operate on nibbles so we will need to do our shifts 4 bits by 4 bits, so 4*nbr_N
 
-
-				uint32_t to_queue_1 = (reverse_rpac_1 << nbr_N) | (packed_query_batch[packed_query_batch_idx + query_batch_regs-1 - i] & ((1<<nbr_N) - 1));
-				uint32_t to_queue_2 = (packed_query_batch[packed_query_batch_idx + query_batch_regs-2 - i] & (0xFFFFFFFF - ((1<<nbr_N) - 1))) | (reverse_rpac_1 >> (32-nbr_N));
-
-				/*				
-				printf("KERNEL DEBUG: rpac_1 Word before reverse: %x, after: %x, split into %x + %x \n", rpac_1, reverse_rpac_1, to_queue_2, to_queue_1 );
-				printf("KERNEL DEBUG: rpac_2 Word before reverse: %x, after: %x\n", rpac_2, reverse_rpac_2 );
-				*/
-
-				packed_query_batch[packed_query_batch_idx + i] = reverse_rpac_2;
-				packed_query_batch[packed_query_batch_idx + query_batch_regs-1 - i] = to_queue_1;
-				if (i!=query_batch_regs_to_swap-1)
-					packed_query_batch[packed_query_batch_idx + query_batch_regs-2 - i] = to_queue_2;
-				
-
-			}
-		}
-
-		if (query_op[tid] & 0x02) // complement
-		{
-			for (uint32_t i = 0; i < (query_batch_regs); i++) // reverse all words. There's a catch with the last word (in the middle of the sequence), see final if.
-			{
-				uint32_t rpac = packed_query_batch[packed_query_batch_idx + i]; //load 8 packed bases from head
-				uint32_t nucleotide = 0;
-
-				#pragma unroll 8
-				for(int k = 28; k >= 0; k = k - 4)		// complement 32-bits word... is pragma-unrolled. 
+				for (uint32_t i = 0; i < *(batch_regs_to_swap); i++) // reverse all words. There's a catch with the last word (in the middle of the sequence), see final if.
 				{
-					nucleotide = (rpac & (0x0F << k)) >> (k);
-					switch(nucleotide)
+					uint32_t rpac_1 = *(packed_batch + *(packed_batch_idx) + i); //load 8 packed bases from head
+					uint32_t rpac_2 = ((*(packed_batch + *(packed_batch_idx) + *(batch_regs)-2 - i)) << (32-nbr_N)) | ((*(packed_batch + *(packed_batch_idx) + *(batch_regs)-1 - i)) >> nbr_N);
+
+
+					uint32_t reverse_rpac_1 = 0;
+					uint32_t reverse_rpac_2 = 0;
+
+
+					#pragma unroll 8
+					for(int k = 28; k >= 0; k = k - 4)		// reverse 32-bits word... is pragma-unrolled. 
 					{
-						case A_PAK:
-							nucleotide = T_PAK;
-						break;
-						case C_PAK:
-							nucleotide = G_PAK;
-						break;
-						case T_PAK:
-							nucleotide = A_PAK;
-						break;
-						case G_PAK:
-							nucleotide = C_PAK;
-						break;
-						default:
-						break;
+						reverse_rpac_1 |= ((rpac_1 & (0x0F << k)) >> (k)) << (28-k);
+						reverse_rpac_2 |= ((rpac_2 & (0x0F << k)) >> (k)) << (28-k);
 					}
-					rpac = (rpac & (0xFFFFFFFF - (0x0F << k))) | nucleotide << k;
-				}
-				printf("KERNEL DEBUG: Word read : %x, after complement: %x\n", packed_query_batch[packed_query_batch_idx + i], rpac);
-				packed_query_batch[packed_query_batch_idx + i] = rpac;
+					// last swap operated manually, because of its irregular size (32 - 4*nbr_N bits, hence 8 - nbr_N nibbles)
 
-			}
+
+					uint32_t to_queue_1 = (reverse_rpac_1 << nbr_N) | ((*(packed_batch + *(packed_batch_idx) + *(batch_regs)-1 - i)) & ((1<<nbr_N) - 1));
+					uint32_t to_queue_2 = ((*(packed_batch + *(packed_batch_idx) + *(batch_regs)-2 - i)) & (0xFFFFFFFF - ((1<<nbr_N) - 1))) | (reverse_rpac_1 >> (32-nbr_N));
+
+					#ifdef DEBUG				
+						printf("KERNEL DEBUG: rpac_1 Word before reverse: %x, after: %x, split into %x + %x \n", rpac_1, reverse_rpac_1, to_queue_2, to_queue_1 );
+						printf("KERNEL DEBUG: rpac_2 Word before reverse: %x, after: %x\n", rpac_2, reverse_rpac_2 );
+					#endif
+
+					*(packed_batch + *(packed_batch_idx) + i) = reverse_rpac_2;
+					(*(packed_batch + *(packed_batch_idx) + *(batch_regs)-1 - i)) = to_queue_1;
+					if (i!=*(batch_regs_to_swap)-1)
+						(*(packed_batch + *(packed_batch_idx) + *(batch_regs)-2 - i)) = to_queue_2;
+					
+
+				} // end for
+			} // end if(reverse)
+
+			if (*(op+tid) & 0x02) // complement
+			{
+				for (uint32_t i = 0; i < *(batch_regs); i++) // reverse all words. There's a catch with the last word (in the middle of the sequence), see final if.
+				{
+					uint32_t rpac = *(packed_batch + *(packed_batch_idx) + i); //load 8 packed bases from head
+					uint32_t nucleotide = 0;
+
+					#pragma unroll 8
+					for(int k = 28; k >= 0; k = k - 4)		// complement 32-bits word... is pragma-unrolled. 
+					{
+						nucleotide = (rpac & (0x0F << k)) >> (k);
+						switch(nucleotide)
+						{
+							case A_PAK:
+								nucleotide = T_PAK;
+							break;
+							case C_PAK:
+								nucleotide = G_PAK;
+							break;
+							case T_PAK:
+								nucleotide = A_PAK;
+							break;
+							case G_PAK:
+								nucleotide = C_PAK;
+							break;
+							default:
+							break;
+						}
+						rpac = (rpac & (0xFFFFFFFF - (0x0F << k))) | nucleotide << k;
+					}
+
+					#ifdef DEBUG
+						printf("KERNEL DEBUG: Word read : %x, after complement: %x\n", *(packed_batch + *(packed_batch_idx) + i), rpac);
+					#endif
+
+					*(packed_batch + *(packed_batch_idx) + i) = rpac;
+
+				} // end for
+			} // end if(complement)
+
 
 
 		}
