7fa660a8988b3d7c30132cf3b48a834b8704423b
J.J.G. (Jonathan) Levy <jlevy@ce-cuda01.ewi.tudelft.nl>
2019-01-18 17:07:38 +0100

    added hardcoded rule for test prog. added crc for current test.

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+TESTPROG_RULE=run2
 ./configure.sh /usr/local/cuda-8.0/
 make GPU_SM_ARCH=sm_35 MAX_SEQ_LEN=300 N_CODE=0x4E N_PENALTY=1
 cd test_prog
-make $1 
+make $TESTPROG_RULE 
 cd ..
 sha256sum -c test_prog/crc_out.log.crc
 
