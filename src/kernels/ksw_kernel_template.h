5c034d6831621544b0b8b8d4952431edf6599f5a
J.J.G. (Jonathan) Levy <jlevy@ce-cuda01.ewi.tudelft.nl>
2019-04-05 17:57:56 +0200

    fixed initialization

--- src/kernels/ksw_kernel_template.h
+++ src/kernels/ksw_kernel_template.h
@@ -19,19 +19,19 @@
 
 
 
+#define CIGAR_MATRIX_SIDE (5)
 
 /* typename meaning : 
     - B is for computing the Second Best Score. Its values are on enum FALSE(0)/TRUE(1).
     (sidenote: it's based on an enum instead of a bool in order to generalize its type from its Int value, with Int2Type meta-programming-template)
 */
-
-
 template <typename B>
 __global__ void gasal_ksw_kernel(uint32_t *packed_query_batch, uint32_t *packed_target_batch,  uint32_t *query_batch_lens, uint32_t *target_batch_lens, uint32_t *query_batch_offsets, uint32_t *target_batch_offsets, uint32_t *seed_score, gasal_res_t *device_res, gasal_res_t *device_res_second, int n_tasks)
 {
+    
+
     const uint32_t tid = (blockIdx.x * blockDim.x) + threadIdx.x;//thread ID
 	if (tid >= n_tasks) return;
-
 	int32_t i, j, k, m, l;
 	int32_t e;
 
@@ -56,7 +56,7 @@ __global__ void gasal_ksw_kernel(uint32_t *packed_query_batch, uint32_t *packed_
 
 	int32_t ridx, gidx;
 	short2 HD;
-	short2 initHD = make_short2(seed_score[tid], 0);
+	short2 initHD = make_short2(seed_score[tid], 0); //copies score from seed.
 	
 	uint32_t packed_target_batch_idx = target_batch_offsets[tid] >> 3; //starting index of the target_batch sequence
 	uint32_t packed_query_batch_idx = query_batch_offsets[tid] >> 3;//starting index of the query_batch sequence
@@ -70,10 +70,34 @@ __global__ void gasal_ksw_kernel(uint32_t *packed_query_batch, uint32_t *packed_
 	int32_t f[9];
 	int32_t p[9];
 	//--------------------------------------------
+
+
+    /*
+    //CIGAR-related matrix that modifies somewhat the score. It seems like it's fixed.
+    //it is defined by opt->a=1 and opt->b=4.
+    int32_t mat[CIGAR_MATRIX_SIDE * CIGAR_MATRIX_SIDE];
+    int a = 1;
+    int b = 4;
+    //copy-pasted from bwa.c:109
+    int _i, _j, _k;
+    for (_i = _k = 0; _i < CIGAR_MATRIX_SIDE-1; ++_i) {
+        for (_j = 0; _j < CIGAR_MATRIX_SIDE-1; ++_j)
+            mat[_k++] = _i == _j? a : -b;
+        mat[_k++] = -1; // ambiguous base
+    }
+    for (j = 0; j < CIGAR_MATRIX_SIDE; ++j) mat[k++] = -1;
+    
+    // generate the query profile
+    int qp[MAX_SEQ_LEN * CIGAR_MATRIX_SIDE];
+	for (_k = _i = 0; _k < CIGAR_MATRIX_SIDE; ++_k) {
+		const int8_t *p = &mat[k * CIGAR_MATRIX_SIDE];
+		for (_j = 0; _j < read_len; ++_j) qp[i++] = p[query[j]];
+	}
+    */
     
     // copies initialization from ksw "fill the first row", line-by-line
     global[0] = initHD;
-    global[1] = make_short2((initHD.x > _cudaGapOE) ? (initHD.x - _cudaGapOE) : (0) , 0);
+    global[1] = make_short2(max(initHD.x - _cudaGapOE, 0) , 0);
     for (i = 2; i < MAX_SEQ_LEN; i++) {
         initHD = make_short2(max(initHD.x - _cudaGapExtend, 0) , 0);
         global[i] = initHD;
@@ -84,12 +108,14 @@ __global__ void gasal_ksw_kernel(uint32_t *packed_query_batch, uint32_t *packed_
         int beg = 0, end = ref_len;
     */
     // bwa does: for (j = beg; LIKELY(j < end); ++j)
+    int32_t u = 0;
 	for (i = 0; i < target_batch_regs; i++) { //target_batch sequence in rows
-		for (m = 0; m < 9; m++) {
-            h[m] = 0;
-            f[m] = 0;
-            p[m] = 0;
-        }
+		for (m = 1; m < 9; m++, u++) {
+			h[m] = max(seed_score[tid] -(_cudaGapO + (_cudaGapExtend*(u))), 0); 
+			f[m] = 0; 
+			p[m] = max(seed_score[tid] -(_cudaGapO + (_cudaGapExtend*(u))), 0); 
+		}
+
 		register uint32_t gpac =packed_target_batch[packed_target_batch_idx + i];//load 8 packed bases from target_batch sequence
 		gidx = i << 3;
 		ridx = 0;
@@ -105,30 +131,29 @@ __global__ void gasal_ksw_kernel(uint32_t *packed_query_batch, uint32_t *packed_
                 h[0] = HD.x;
                 e = HD.y;
 
-
                 register int32_t prev_hm_diff = h[0] - _cudaGapOE;
                 #pragma unroll 8
                 for (l = 28, m = 1; m < 9; l -= 4, m++) {
-                            uint32_t gbase = (gpac >> l) & 15; /* get a base from target_batch sequence */ 
-                            DEV_GET_SUB_SCORE_LOCAL(subScore, rbase, gbase);/* check equality of rbase and gbase */
-                            register int32_t curr_hm_diff = h[m] - _cudaGapOE;
-                            f[m] = max(curr_hm_diff, f[m] - _cudaGapExtend);/* whether to introduce or extend a gap in query_batch sequence */
-                            curr_hm_diff = p[m] + subScore;/* score if rbase is aligned to gbase */
-                            curr_hm_diff = max(curr_hm_diff, f[m]);
-                            curr_hm_diff = max(curr_hm_diff, 0);
-                            e = max(prev_hm_diff, e - _cudaGapExtend);/* whether to introduce or extend a gap in target_batch sequence */
-                            curr_hm_diff = max(curr_hm_diff, e);
-                            maxXY_y = (maxHH < curr_hm_diff) ? gidx + (m-1) : maxXY_y; 
-                            maxHH = (maxHH < curr_hm_diff) ? curr_hm_diff : maxHH;
-                            h[m] = curr_hm_diff;
-                            p[m] = prev_hm_diff + _cudaGapOE;
-                            prev_hm_diff=curr_hm_diff - _cudaGapOE;
-                        if (SAMETYPE(B, Int2Type<TRUE>))
-                        {
-                            bool override_second = (maxHH_second < curr_hm_diff) && (maxHH > curr_hm_diff);
-                            maxXY_y_second = (override_second) ? gidx + (m-1) : maxXY_y_second; 
-                            maxHH_second = (override_second) ? curr_hm_diff : maxHH_second;
-                        }
+                    uint32_t gbase = (gpac >> l) & 15; /* get a base from target_batch sequence */ 
+                    DEV_GET_SUB_SCORE_LOCAL(subScore, rbase, gbase);/* check equality of rbase and gbase */
+                    register int32_t curr_hm_diff = h[m] - _cudaGapOE;
+                    f[m] = max(curr_hm_diff, f[m] - _cudaGapExtend);/* whether to introduce or extend a gap in query_batch sequence */
+                    curr_hm_diff = p[m] + subScore;/* score if rbase is aligned to gbase */
+                    curr_hm_diff = max(curr_hm_diff, f[m]);
+                    curr_hm_diff = max(curr_hm_diff, 0);
+                    e = max(prev_hm_diff, e - _cudaGapExtend);/* whether to introduce or extend a gap in target_batch sequence */
+                    curr_hm_diff = max(curr_hm_diff, e);
+                    maxXY_y = (maxHH < curr_hm_diff) ? gidx + (m-1) : maxXY_y; 
+                    maxHH = (maxHH < curr_hm_diff) ? curr_hm_diff : maxHH;
+                    h[m] = curr_hm_diff;
+                    p[m] = prev_hm_diff + _cudaGapOE;
+                    prev_hm_diff=curr_hm_diff - _cudaGapOE;
+                    if (SAMETYPE(B, Int2Type<TRUE>))
+                    {
+                        bool override_second = (maxHH_second < curr_hm_diff) && (maxHH > curr_hm_diff);
+                        maxXY_y_second = (override_second) ? gidx + (m-1) : maxXY_y_second; 
+                        maxHH_second = (override_second) ? curr_hm_diff : maxHH_second;
+                    }
                 }
 
                 //----------save intermediate values------------
