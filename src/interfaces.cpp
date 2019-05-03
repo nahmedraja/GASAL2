e3ec142889e8cd167fa1061df388ce3cc58d4ddd
J.J.G. (Jonathan) Levy <jlevy@ce-cuda01.ewi.tudelft.nl>
2019-05-03 17:54:00 +0200

    switched to GTX 750 Ti

--- src/interfaces.cpp
+++ src/interfaces.cpp
@@ -86,28 +86,34 @@ void gasal_op_fill(gasal_gpu_storage_t *gpu_storage_t, uint8_t *data, uint32_t n
 	memcpy(host_op, data, nbr_seqs_in_stream);
 }
 
-
-void gasal_set_device(int gpu_select)
+void gasal_set_device(int gpu_select, bool isPrintingProp)
 {
 	/* 
 	Select GPU
 	*/
-	int num_devices, device;
-	cudaGetDeviceCount(&num_devices);
-	fprintf(stderr, "Found %d GPUs\n", num_devices);
-	if (gpu_select  > num_devices-1)
+	if (isPrintingProp)
 	{
-		fprintf(stderr, "Error: can't select device %d when only %d devices are selected (range from 0 to %d)\n", gpu_select, num_devices, num_devices-1);
-		exit(EXIT_FAILURE);
-	}
-	if (num_devices > 0) {
-		cudaDeviceProp properties;
-		for (device = 0; device < num_devices; device++) {
-				cudaGetDeviceProperties(&properties, device);
-				fprintf(stderr, "\tGPU %d: %s\n", device, properties.name);
+		int num_devices, device;
+		cudaGetDeviceCount(&num_devices);
+		fprintf(stderr, "Found %d GPUs\n", num_devices);
+		if (gpu_select  > num_devices-1)
+		{
+			fprintf(stderr, "Error: can't select device %d when only %d devices are selected (range from 0 to %d)\n", gpu_select, num_devices, num_devices-1);
+			exit(EXIT_FAILURE);
 		}
-		cudaGetDeviceProperties(&properties, gpu_select);
-		fprintf(stderr, "Selected device %d : %s\n", gpu_select, properties.name);
+		if (num_devices > 0) {
+			cudaDeviceProp properties;
+			for (device = 0; device < num_devices; device++) {
+					cudaGetDeviceProperties(&properties, device);
+					fprintf(stderr, "\tGPU %d: %s\n", device, properties.name);
+			}
+			cudaGetDeviceProperties(&properties, gpu_select);
+			fprintf(stderr, "Selected device %d : %s\n", gpu_select, properties.name);
+			cudaSetDevice(gpu_select);
+		}
+	} else {
+		// silently select device
 		cudaSetDevice(gpu_select);
 	}
+
 }
