4a7142bb58a1c37d9a01ebfa225549dc86b23589
J.J.G. (Jonathan) Levy <jlevy@ce-cuda01.ewi.tudelft.nl>
2019-05-16 17:21:03 +0200

    switched to GPU 0 as GPU 1 has been removed

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./configure.sh /usr/local/cuda-10.0/
-make GPU_SM_ARCH=sm_50 MAX_SEQ_LEN=153 N_CODE=4 N_PENALTY=1
+make GPU_SM_ARCH=sm_35 MAX_SEQ_LEN=153 N_CODE=4 N_PENALTY=1
 cd test_prog
 make 262k2 
 sha256sum -c crc_out.log.crc
