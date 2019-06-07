57490bd975d88d514159efed68c002e5b1242db8
J.J.G. (Jonathan) Levy <jlevy@ce-cuda01.ewi.tudelft.nl>
2019-06-07 17:38:43 +0200

    Fixed reallocation for regular fields. Should work.

--- src/interfaces.cpp
+++ src/interfaces.cpp
@@ -38,7 +38,6 @@ void gasal_host_alns_resize(gasal_gpu_storage_t *gpu_storage, int new_max_alns, 
 	// don't care about realloc'ing gpu-sided fields as they will be taken care of before aligning.
 
 	gpu_storage->host_query_op = (uint8_t*) cudaHostRealloc<uint8_t>((void*) gpu_storage->host_query_op, new_max_alns, gpu_storage->host_max_n_alns);
-	
 	gpu_storage->host_target_op = (uint8_t*) cudaHostRealloc<uint8_t>((void*) gpu_storage->host_target_op, new_max_alns, gpu_storage->host_max_n_alns);
 	
 	if (params->algo == KSW)
@@ -55,12 +54,22 @@ void gasal_host_alns_resize(gasal_gpu_storage_t *gpu_storage, int new_max_alns, 
 	
 	gasal_res_destroy_host(gpu_storage->host_res);
 	gpu_storage->host_res = gasal_res_new_host(new_max_alns, params);
-	
+	gpu_storage->device_cpy = gasal_res_new_device_cpy(new_max_alns, params);
+	gpu_storage->device_res = gasal_res_new_device(gpu_storage->device_cpy);
+
 	if (params->secondBest)
 	{	
 		gasal_res_destroy_host(gpu_storage->host_res_second);
 		gpu_storage->host_res_second = gasal_res_new_host(new_max_alns, params);
+		gpu_storage->device_cpy_second = gasal_res_new_device_cpy(new_max_alns, params);
+		gpu_storage->device_res_second = gasal_res_new_device(gpu_storage->device_cpy_second);
+
+	} else {
+		gpu_storage->host_res_second = NULL;
+		gpu_storage->device_cpy_second = NULL;
+		gpu_storage->device_res_second = NULL;
 	}
+	
 	//fprintf(stderr, "_res done ");
 
 	gpu_storage->host_max_n_alns = new_max_alns;
--- test_prog/test_prog.cpp
+++ test_prog/test_prog.cpp
@@ -230,12 +230,12 @@ int main(int argc, char **argv) {
 		//initializing the streams by allocating the required CPU and GPU memory
 		// note: the calculations of the detailed sizes to allocate could be done on the library side (to hide it from the user's perspective)
 		gasal_init_streams(&(gpu_storage_vecs[z]), 
-						0.02 * (maximum_sequence_length_query + 7) * GPU_BATCH_SIZE , //TODO: remove maximum_sequence_length_query
-						0.1 * (maximum_sequence_length + 7) * GPU_BATCH_SIZE , 
+						0.01 * (maximum_sequence_length_query + 7) * GPU_BATCH_SIZE , //TODO: remove maximum_sequence_length_query
+						0.04 * (maximum_sequence_length + 7) * GPU_BATCH_SIZE , 
 						0.03 * (maximum_sequence_length + 7) * GPU_BATCH_SIZE ,
-						0.2 * (maximum_sequence_length + 7) * GPU_BATCH_SIZE , 
-						GPU_BATCH_SIZE, // maximum number of alignments is bigger on target than on query side.
-						GPU_BATCH_SIZE, 
+						0.01 * (maximum_sequence_length + 7) * GPU_BATCH_SIZE , 
+						0.02 * GPU_BATCH_SIZE, //host // maximum number of alignments is bigger on target than on query side.
+						0.001 * GPU_BATCH_SIZE, //device
 						args);
 	}
 	#ifdef DEBUG
