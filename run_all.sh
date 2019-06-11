f39ca587785b0900156f266bc54e122a6d86c3a6
J.J.G. (Jonathan) Levy <jlevy@ce-cuda01.ewi.tudelft.nl>
2019-06-11 11:57:35 +0200

    switched back to N_CODE=4

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./configure.sh /usr/local/cuda-10.0/
-make GPU_SM_ARCH=sm_35 MAX_SEQ_LEN=153 N_CODE=0x4E N_PENALTY=1
+make GPU_SM_ARCH=sm_35 MAX_SEQ_LEN=153 N_CODE=4 N_PENALTY=1
 cd test_prog
 make $1
 sleep 5s
